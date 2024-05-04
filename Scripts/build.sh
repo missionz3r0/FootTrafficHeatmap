f0f9fbfac112c40ef9c524155d794b1acdf048b5
missionz3r0 <github_xu8w@missionz3r0.com>
2024-05-04 03:56:33 -0400

    change: move csproj files into project directory

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Build the assemblies using a containerized dotnet
-podman run --rm -v $PWD:/mod dotnet build ./Source/FootTrafficHeatmap.sln --framework net472 --configuration Release  "$@"
+podman run --rm -v $PWD:/mod dotnet build ./Source/Mod.sln --framework net472 --configuration Release  "$@"
